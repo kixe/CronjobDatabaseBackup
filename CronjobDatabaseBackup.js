4ed6ed21a469a5bdc8b8029aae0e2f7c14182f2d
Qualyweb <note@qualyweb.com>
2019-10-27 18:09:46 +0100

    update 119
    added option select/ deselect all tables in one click for individual selection of tables

---
+++ CronjobDatabaseBackup.js
@@ -0,0 +1,31 @@
+jQuery(document).ready(function($) {
+	$('.InputfieldCheckboxes input:checkbox.check').click(function() {
+		var checked = !$(this).data('checked');
+		var parent = $(this).data('for');
+		var boxes = $(parent + '.InputfieldCheckboxes .InputfieldCheckboxesColumns input:checkbox[disabled!=\'disabled\']');
+		var cboxes = $('.Inputfield_tables_content.InputfieldCheckboxes .InputfieldCheckboxesColumns input:checkbox');
+    	boxes.prop('checked', checked);
+    	if (parent == '.Inputfield_tables') {
+    		if (checked) {
+    			cboxes.prop('checked', checked);
+    			cboxes.removeAttr('disabled');
+    		} else {
+				cboxes.prop('checked', checked);
+				cboxes.attr('disabled','disabled');
+    		}
+    	}
+		$(this).val(checked ? 1 : 0 )
+		$(this).data('checked', checked);
+    });
+    $('.Inputfield_tables.InputfieldCheckboxes .InputfieldCheckboxesColumns input:checkbox').click(function() {
+		var val = $(this).val();
+		var checked = $(this).prop('checked');
+		var box = $('.Inputfield_tables_content.InputfieldCheckboxes .InputfieldCheckboxesColumns input:checkbox[value=\'' + val + '\']');
+		if (!checked) {
+			box.prop('checked', checked);
+			box.attr('disabled','disabled');
+		} else {
+			box.removeAttr('disabled');
+		}
+    });
+});
